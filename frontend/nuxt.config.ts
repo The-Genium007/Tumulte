280d943b3cef6cf3d771b2f9f519f90219180ff8
The-Genium007 <genium007@gmail.com>
2026-01-02 14:52:31 +0100

    nuxt SPA mode

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  ssr: false, // SPA mode - variables must be set at build time
 
   modules: ["@nuxt/ui", "@pinia/nuxt", "@vite-pwa/nuxt"],
 
